63a74290737f8cb6844c8ef7013d6af7eeed8da6
Isaac <hizikugwuanya@gmail.com>
2023-09-16 23:39:55 +0100

    chore: setting up sequelize configuration

---
+++ src/core/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface UserAttributes {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    phone: string;
+    bio: string;
+    image: string;
+    isVerified: boolean;
+    isAdmin: boolean;
+    reputation: number;
+}
